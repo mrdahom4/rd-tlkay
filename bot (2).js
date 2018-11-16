a8fc1f2e5c1e702ead5f5e1ea57a5dc923489d42
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-11-16 13:25:27 +0200

    Update bot (2).js

--- bot (2).js
+++ bot (2).js
@@ -1,20 +1,61 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = "-";
+client.on("message", message => {
 
-client.on('message', msg => {
-  if(msg.content === 'هلا')
-              setTimeout(function() {  
-msg.reply('**هلا حبي**')
-              }, 00)
+            if (message.content.startsWith(prefix + "Nbc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
- 
- client.on('message', msg => {
-  if(msg.content === 'السلام عليكم')
-              setTimeout(function() {  
-msg.reply('**وعليكم السلام ورحمه الله وبركاته**')
-              }, 00)
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Simo New`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
 
 
+const devs = ['487727064192122880' , '' , '' , ''];
+const adminprefix = "N-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'pla')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'watching'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+  });
 
-client.login("NDg3NzI3MDY0MTkyMTIyODgw.DscrwQ.TZYt6JgUGlEcjPCDu-2EKs-r4zY");
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
