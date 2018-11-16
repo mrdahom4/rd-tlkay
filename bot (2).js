01df901053a321226d52f5560dd53fa8d0692e20
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-11-16 13:10:12 +0200

    Update bot (2).js

--- bot (2).js
+++ bot (2).js
@@ -1,23 +1,20 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const YTDL = require('ytdl-core');
-const nodeopus = require('node-opus');
-const ffmpeg = require('ffmpeg');
-var servers = {};
-function play(connection, message, args) {
-  var server = servers[message.guild.id];
-  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
-  server.queue.shift();
-  server.dispatcher.on("end", function() {
-    if (server.queue[0]) play(connection, message);
-    else connection.disconnect();
-  });
-}
+
+client.on('message', msg => {
+  if(msg.content === 'هلا')
+              setTimeout(function() {  
+msg.reply('**هلا حبي**')
+              }, 00)
+});
+ 
+ client.on('message', msg => {
+  if(msg.content === 'السلام عليكم')
+              setTimeout(function() {  
+msg.reply('**وعليكم السلام ورحمه الله وبركاته**')
+              }, 00)
+});
 
 
-client.on('message', message =>{
-  if(message.content.startsWith('join')){
-    const voiceChannel = message.member.voiceChannel
-    voiceChannel.join();
-    message.channel.send("تم الأتصال بالروم الصوتي")
-}})
+
+client.login("NDg3NzI3MDY0MTkyMTIyODgw.DscrwQ.TZYt6JgUGlEcjPCDu-2EKs-r4zY");
